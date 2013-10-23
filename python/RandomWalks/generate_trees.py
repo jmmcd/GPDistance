fa5d6083adea1e7ec7dcc3ebe7fe3f2198f13090
jmmcd <jamesmichaelmcdermott@gmail.com>
2013-10-23 16:22:21 +0100

    Added functions for getting the number of trees of a given shape and number of shapes of given depth.

--- python/RandomWalks/generate_trees.py
+++ python/RandomWalks/generate_trees.py
@@ -3,13 +3,7 @@
 import sys
 from itertools import product
 
-vars = list("xy")
-fns = {"+": 2, "-": 2, "*": 2, "/": 2}
-
-# Given this language, there are 2 trees of depth 0, 18 of depth 1,
-# 1298 of depth 2, and 6739218 of depth 3. 
-
-def trees_of_depth(n):
+def trees_of_depth(n, vars, fns):
     """Generate all trees of exactly depth n, along with their
     depths."""
     if n == 0:
@@ -17,24 +11,26 @@ def trees_of_depth(n):
             yield item, 0
     else:
         for fn in fns:
-            for children in product(trees_of_depth_LE(n-1), repeat=fns[fn]):
+            for children in product(
+                trees_of_depth_LE(n-1, vars, fns), repeat=fns[fn]):
                 if any(child[1] == n-1 for child in children):
                     yield ("(" + fn + " "
                            + " ".join(child[0] for child in children)
                            + ")"), n
                     
-def trees_of_depth_LE(n):
+def trees_of_depth_LE(n, vars, fns):
     """Generate all trees up to and including depth n, along with
     their depths."""
     for d in range(n+1):
-        for item, d1 in trees_of_depth(d):
+        for item, d1 in trees_of_depth(d, vars, fns):
             yield item, d1
 
-def count_trees_of_depth_LE(n):
+def count_trees_of_depth_LE(n, vars, fns):
     """Count the number of trees of depth less than or equal to n."""
-    return sum(count_trees_of_depth(i) for i in range(n+1))
+    return sum(count_trees_of_depth(i, vars, fns)
+               for i in range(n+1))
             
-def count_trees_of_depth(n):
+def count_trees_of_depth(n, vars, fns):
     """Count the trees of depth exactly n."""
     if n == 0:
         return len(vars)
@@ -52,14 +48,43 @@ def count_trees_of_depth(n):
         # or you can do vice versa (right subtree of depth n-1, left
         # subtree of any depth less than that). so multiply previous
         # quantity by 2.
-        return len(fns.keys()) * (count_trees_of_depth(n-1) ** 2 + sum(
-            2 * count_trees_of_depth(n-1) * count_trees_of_depth(m)
-            for m in range(n-1)))
+        return len(fns.keys()) * (
+            count_trees_of_depth(n-1, vars, fns) **
+            2 + sum(2 * count_trees_of_depth(n-1)
+                    * count_trees_of_depth(m)
+                    for m in range(n-1)))
+
+def count_trees_of_given_shape(t, vars, fns):
+    """Tree t is a string, eg (* (+ x y) y). This has n = 2 internal
+    nodes, m = 3 leaves. Number of trees of the same shape will be
+    4^n2^m (if there are 4 possibilities for internal nodes and 2 for
+    leaves)."""
+    def noccurrences(s1, s2):
+        """How many times do items in s2 occur in s1?"""
+        return sum(s1.count(a) for a in s2)
+    internal = vars
+    external = "".join(fns.keys())
+    n = noccurrences(t, internal)
+    m = noccurrences(t, external)
+    return (len(internal)**n) * (len(external)**m)
+
+def shapes_of_depth_LE(n):
+    for t in trees_of_depth_LE(n, "x", {"*": 2}): yield t
 
 if __name__ == "__main__":
+
+    # Given this language, there are 2 trees of depth 0, 18 of depth
+    # 1, 1298 of depth 2, and 6739218 of depth 3.
+    vars = list("xy")
+    fns = {"+": 2, "-": 2, "*": 2, "/": 2}
+    
     n = int(sys.argv[1])
     if len(sys.argv) > 2 and sys.argv[2] == "enumerate":
-        for item, d in trees_of_depth_LE(n):
+        for item, d in trees_of_depth_LE(n, vars, fns):
             print(item)
+    elif len(sys.argv) > 2 and sys.argv[2] == "shapes":
+        for item, d in shapes_of_depth_LE(n):
+            print("%d: %s" % (count_trees_of_given_shape(item, vars, fns),
+                             item))
     else:
-        print(count_trees_of_depth_LE(n))
+        print(count_trees_of_depth_LE(n, vars, fns))
