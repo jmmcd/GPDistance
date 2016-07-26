c73a1f7880d0eeb5dc38b274b3e6c9b1845ac040
James McDermott <jamesmichaelmcdermott@gmail.com>
2016-07-26 22:53:22 +0100

    Added fns for abstract space.

--- python/RandomWalks/random_walks.py
+++ python/RandomWalks/random_walks.py
@@ -623,6 +623,28 @@ def detailed_balance(tp, s=None):
     f = tp * s.reshape((len(s), 1))
     return is_symmetric(f)
 
+def int_space_make_ops(N):
+    def uniform(x):
+        return random.randrange(N)
+    def ten_percent(x):
+        width = N / 10
+        choice = random.randrange(-width, width)
+        return (x + choice) % N
+    def deterministic(x):
+        return (x+1) % N
+    return uniform, ten_percent, deterministic
+
+def int_space_make_rows(N):
+    x_unif = np.ones(N) / float(N)
+
+    x_10pc = np.zeros(N)
+    width = N / 10
+    x_10pc[0:width] = 1.0 / width
+
+    x_det = np.zeros(N)
+    x_det[0] = 1.0
+
+    return x_unif, x_10pc, x_det
 
 if __name__ == "__main__":
     dirname = sys.argv[1]
